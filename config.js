16799843ec1022effe62533ae19c3f9cbef93111
Ahmed Shariar Hossain Tasnim <ahmedshariarhossaintasnim@gmail.com>
2026-09-04 00:17:01 +0600

    Initialize Firebase configuration in config.js
    Add Firebase configuration settings for the project.

---
+++ config.js
@@ -0,0 +1,14 @@
+// ═══════════════════════════════════════════════════════════
+// FILE: config.js - একবার Firebase কনফিগ বসাও
+// ═══════════════════════════════════════════════════════════
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyAQGuCsA0ZWG877wX-cC2P_YRESKoqbRnw",
+  authDomain: "chatme-ab9a2.firebaseapp.com",
+  databaseURL: "https://chatme-ab9a2-default-rtdb.firebaseio.com",
+  projectId: "chatme-ab9a2",
+  storageBucket: "chatme-ab9a2.firebasestorage.app",
+  messagingSenderId: "986243476700",
+  appId: "1:986243476700:web:f0157d03131edda1269819",
+  measurementId: "G-HXPVBG6768"
+};
